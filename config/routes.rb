08813255004848cdd8c2b28860cf3b11e837f0a8
wenqinYe <samcarbet@Wenqins-MacBook-Pro.local>
2014-05-19 20:46:28 -0400

    added full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ SampleApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,25 +1,34 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home Page" do
-    it "should have the right title" do
-    	visit '/static_pages/home'
-    	expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
-    end
-
-    describe "Help Page" do
-    	it "should have the right title for about" do
-    		visit '/static_pages/help'
-    		expect(page).to have_content('Ruby on Rails Tutorial Sample App | Help')
+  	describe "Home Page" do
+    	it "should not have home in title" do
+    		visit '/static_pages/home'
+    		expect(page).not_to have_title('| Home')
     	end
-    end
 
-    describe "About page" do
-    	it "should have the right content title" do
-    		visit '/static_pages/about'
-    		expect(page).to have_content("Ruby on Rails Tutorial Sample App | About")
+    	describe "Help Page" do
+    		it "should have the right title for about" do
+    			visit '/static_pages/help'
+    			expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    		end
     	end
-    end
+
+    	describe "About page" do
+    		it "should have the right content title" do
+    			visit '/static_pages/about'
+    			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    		end
+    	end
+
+    	describe "Contact Page" do
+    		it "should have the right content title" do
+    			visit '/static_pages/contact'
+    			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    		end
+    	end
+
+
 
   end
 end
